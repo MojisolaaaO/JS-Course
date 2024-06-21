843889f7e87d58ef2a319d963eacb049bb42d5e3
MojisolaaaO <mojisolaotusheso@gmail.com>
2024-06-21 09:38:56 +0100

    updates

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+//displays an alert box with the text 'Hello World!
+alert('Hello World!')
+
 //declaring a variable named "myCourse" and setting its value to "Web Automation with JavaScript"
 const myCourse = 'Web Automation with JavaScript'
 
-//displaying the value in a dialog box
-alert(myCourse)
+//Outputs the value of myCourse to the console
+console.log(myCourse)
