3964fd1b5c68ad9fc10389bfaf90ec6e2d07f617
MojisolaaaO <mojisolaotusheso@gmail.com>
2024-06-24 22:05:38 +0100

    task 5

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ const position = '1st'
 
 const output = 'Today is ' + day + '.' + ' It is the ' + position + ' day of the week' + '. '
 
-//prints the value of output to the console
+//logs the value of output to the console
 console.log(output)
